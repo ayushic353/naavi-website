06bbdfa7eb659cfba7a3bde94cef80761da1cb28
ayushic3 <aayushicharde@gmail.com>
2026-04-19 10:54:36 +0530

    Connect frontend to live backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://naavi-website.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 });
 
